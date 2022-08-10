72530b0adebe7b1247df63863f21deb037493886
Sigismundo4321 <sigismundo4321@gmail.com>
2022-08-09 21:09:15 -0300

    :construction: :bug:  melhoria no nome

--- lib/app/Domain/repositories_interface/watch_live/search_watch_live_repositories_interface.dart
+++
@@ -1,6 +0,0 @@
-import 'package:either_dart/either.dart';
-import 'package:game_streaming/app/Domain/entities/watch_live.dart';
-
-abstract class SearchWatchLiveRepositoriesInterface {
-  Future<Either<String, WatchLive>> call();
-}
--- lib/app/Domain/usecases/watch_live/search_watch_live_usecase_imp.dart
+++ lib/app/Domain/usecases/watch_live/search_watch_live_usecase_imp.dart
@@ -1,6 +1,6 @@
 import 'package:either_dart/either.dart';
 import 'package:game_streaming/app/Domain/entities/watch_live.dart';
-import 'package:game_streaming/app/Domain/repositories_interface/watch_live/search_watch_live_repositories_interface.dart';
+import 'package:game_streaming/app/Domain/repositories_interface/search_watch_live_repositories_interface.dart';
 import 'package:game_streaming/app/Domain/usecases/watch_live/search_watch_live_usecase_interface.dart';
 
 class SearchWatchLiveImp implements SearchWatchLiveUseCaseInterface {
