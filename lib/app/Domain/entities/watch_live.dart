387ee3ef91d40b4ff7faf99990492309bb45e244
Sigismundo4321 <sigismundo4321@gmail.com>
2022-08-12 07:58:34 -0300

    :sparkles: criando regra de uso

--- lib/app/Domain/entities/watch_live.dart
+++ lib/app/Domain/entities/watch_live.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:game_streaming/app/domain/entities/game_categories.dart';
 import 'package:game_streaming/app/domain/entities/game_live.dart';
 
@@ -8,4 +10,41 @@ class WatchLive {
     required this.listGameLive,
     required this.listGameCategorie,
   });
+
+  List<GameLive> randomGame() {
+    Random random = Random();
+    int amount = 5;
+    List<GameLive> listGameRandom = [];
+
+    for (int index = 0; index < amount; index++) {
+      listGameRandom.add(listGameLive[random.nextInt(listGameLive.length)]);
+    }
+
+    return listGameRandom;
+  }
+
+  List<GameLive> topStreamers() {
+    List<GameLive> topStreamers = [];
+
+    for (var element in listGameLive) {
+      if (element.isTopStreamers) {
+        topStreamers.add(element);
+      }
+    }
+
+    return topStreamers;
+  }
+
+  List<GameCategories> randomGameCategories() {
+    Random random = Random();
+    int amount = 5;
+    List<GameCategories> listGameCategoriesRandom = [];
+
+    for (int index = 0; index < amount; index++) {
+      listGameCategoriesRandom
+          .add(listGameCategorie[random.nextInt(listGameCategorie.length)]);
+    }
+
+    return listGameCategoriesRandom;
+  }
 }
